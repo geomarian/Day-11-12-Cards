6de8149d9985ebc5bdcd0ebe7d94236746931d27
geomarian <geomarian520@gmail.com>
2022-09-18 23:25:52 +0530

    Added Deck of cards file

---
+++ src/com/bridgelabz/DeckOfCards.java
@@ -0,0 +1,39 @@
+/**
+ * 
+ */
+package com.bridgelabz;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+/**
+ *
+ * 
+ * 
+ * 
+ * @author ASUS
+ *
+ */
+public class DeckOfCards {
+
+	/**
+	 * @param args
+	 */
+	
+	
+	static Random random = new Random();
+	
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		Cards cardDeck = new Cards();
+		cardDeck.cardsShuffle();
+		
+		
+		
+	}
+
+}
